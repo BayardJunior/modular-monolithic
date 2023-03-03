da7e261960fa22952d45e61f85d18db2b7c5d007
Bayard Junior <sobreira.b@gmail.com>
2023-03-03 18:23:58 -0300

    Criada factory para simplificar o uso da minha facade nos demais modulos

---
+++ src/modules/product-adm/facade/product-adm.facade.factory.ts
@@ -0,0 +1,16 @@
+import ProductRepository from "../repository/product.repository";
+import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import ProductAdmFacade from "./product-adm.facade";
+
+export default class ProductAdmFacadeFactory {
+  static create() {
+    const productRepository = new ProductRepository();
+    const addProductUseCase = new AddProductUseCase(productRepository);
+    const productFacade = new ProductAdmFacade({
+      addUseCase: addProductUseCase,
+      stockUseCase: undefined,
+    });
+
+    return productFacade;
+  }
+}
--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -3,6 +3,7 @@ import ProductModel from "../repository/product.model";
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
 import ProductAdmFacade from "./product-adm.facade";
+import ProductAdmFacadeFactory from "./product-adm.facade.factory";
 
 describe("ProductAdmFacade test", () => {
   let sequelize: Sequelize;
@@ -22,12 +23,12 @@ describe("ProductAdmFacade test", () => {
   });
 
   it("should create a product", async () => {
-    const productRepository = new ProductRepository();
-    const addProductUseCase = new AddProductUseCase(productRepository);
-    const productFacade = new ProductAdmFacade({
-      addUseCase: addProductUseCase,
-      stockUseCase: undefined,
-    });
+    // const productRepository = new ProductRepository();
+    // const addProductUseCase = new AddProductUseCase(productRepository);
+    // const productFacade = new ProductAdmFacade({
+    //   addUseCase: addProductUseCase,
+    //   stockUseCase: undefined,
+    // });
 
     const input = {
       id: "1",
@@ -36,6 +37,8 @@ describe("ProductAdmFacade test", () => {
       purchasePrice: 100,
       stock: 10,
     };
+
+    const productFacade = ProductAdmFacadeFactory.create();
 
     await productFacade.addProduct(input);
     const product = await ProductModel.findOne({ where: { id: "1" } });
